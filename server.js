0fa10be3b33b24313ada73294f80bfc65f83b765
Your Name <you@example.com>
2026-03-20 21:40:34 +0530

    Fix Vercel deployment

--- server.js
+++ server.js
@@ -253,6 +253,10 @@ app.post("/weather", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(3000, () => {
+      console.log("Server running at http://localhost:3000");
+    });
+  }
+  
+  module.exports = app;
